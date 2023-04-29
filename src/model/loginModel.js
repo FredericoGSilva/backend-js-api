01b4b910f85f5e44256a8f0a67ab12d7a3506271
FredericoGSilva <frederico.dev@outlook.com>
2023-04-28 22:46:13 -0300

    listLoginById - Listar login por id

--- src/model/loginModel.js
+++ src/model/loginModel.js
@@ -4,6 +4,11 @@ const loginModel = {
     listLogin: (callback) => {
         const sql = "select * from login"
         pool.query(sql, callback)
+    },
+
+    listLoginById: (id, callback) => {
+        const sql = "select * from login where id_login = ?"
+        pool.query(sql, [id], callback)
     }
 }
 

187a84f5ad11d8ae46b6a24736202c4a3416d26a
FredericoGSilva <frederico.dev@outlook.com>
2023-04-28 21:49:56 -0300

    loginRoute criado

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from "express"
-//import routes from "./routes/loginRoute.js"
+import router from "./routes/loginRoute.js"
 
 const app = express()
 
 app.use(express.json())
-//app.use(routes)
+app.use(router)
 
 export default app
 
